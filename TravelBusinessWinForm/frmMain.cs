d153402c0ffffe79d69d2d5f5a956cc97fae97c9
AlexKLBuckley1995 <alextravel1995@gmail.com>
2017-06-07 16:19:41 +1200

    Commit: Working display of the locations on frmMain

---
+++ TravelBusinessWinForm/frmMain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TravelBusinessWinForm
+{
+    public sealed partial class frmMain : Form
+    {
+        private static readonly frmMain _Instance = new frmMain();
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        async public void UpdateDisplay()
+        {
+            try
+            {
+                lstItemType.DataSource = null;
+                lstItemType.DataSource = await ServiceClient.GetLocationNamesAsync();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            UpdateDisplay();
+        }
+    }
+}
---
+++ TravelItemAppSelfHost/TravelShopController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelItemAppSelfHost
+{
+    public class TravelShopController : System.Web.Http.ApiController
+    {
+        public List<string> GetLocationNames()
+        {
+            DataTable lcResult = clsDbConnection.GetDataTable("SELECT LocationName FROM Location", null);
+            List<string> lcLocationNames = new List<string>();
+            foreach (DataRow dr in lcResult.Rows)
+                lcLocationNames.Add((string)dr[0]);
+                return lcLocationNames;
+        }
+    }
+}
